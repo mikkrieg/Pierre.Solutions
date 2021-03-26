238aef2584f038565295531304c660b13dfe7602
Mikey Kriegel <mikkrieg@gmail.com>
2021-03-26 14:29:42 -0700

    Create Index and Create routes and their views for treats

--- Pierre/Controllers/TreatsController.cs
+++ Pierre/Controllers/TreatsController.cs
@@ -3,5 +3,44 @@ using Pierre.Models;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
+
+namespace Pierre.Controllers
+{
+  public class TreatsController : Controllers
+  {
+    private readonly PierreContext _db;
+
+    public CategoriesController(PierreContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      List<Treat> Treats = _db.Treats.ToList();
+      List<Flavor> Flavors = _db.Flavors.ToList();
+      model.Add("Treats", Treats);
+      model.Add("Flavors", Flavors);
+      return View(model);
+    }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Treat treat)
+    {
+      _db.Treats.Add(treat);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+  }
+}
